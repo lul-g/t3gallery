0d861ef115c5923d70a75afea7f990281ec0487e
Lulseged Admasu <lulsegedwork@gmail.com>
2024-05-16 11:13:52 -0400

    add loading spinner | uploading toast

--- src/common/UploadButtonCustom.tsx
+++ src/common/UploadButtonCustom.tsx
@@ -28,6 +28,76 @@ function UploadSVG() {
   );
 }
 
+const LoadingSpinner = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="white"
+    >
+      <rect className="spinner_zWVm" x="1" y="1" width="7.33" height="7.33" />
+      <rect
+        className="spinner_gfyD"
+        x="8.33"
+        y="1"
+        width="7.33"
+        height="7.33"
+      />
+      <rect
+        className="spinner_T5JJ"
+        x="1"
+        y="8.33"
+        width="7.33"
+        height="7.33"
+      />
+      <rect
+        className="spinner_E3Wz"
+        x="15.66"
+        y="1"
+        width="7.33"
+        height="7.33"
+      />
+      <rect
+        className="spinner_g2vs"
+        x="8.33"
+        y="8.33"
+        width="7.33"
+        height="7.33"
+      />
+      <rect
+        className="spinner_ctYB"
+        x="1"
+        y="15.66"
+        width="7.33"
+        height="7.33"
+      />
+      <rect
+        className="spinner_BDNj"
+        x="15.66"
+        y="8.33"
+        width="7.33"
+        height="7.33"
+      />
+      <rect
+        className="spinner_rCw3"
+        x="8.33"
+        y="15.66"
+        width="7.33"
+        height="7.33"
+      />
+      <rect
+        className="spinner_Rszm"
+        x="15.66"
+        y="15.66"
+        width="7.33"
+        height="7.33"
+      />
+    </svg>
+  );
+};
+
 const useUploadThingInputProps = (...args: Input) => {
   const $ut = useUploadThing(...args);
 
@@ -54,10 +124,15 @@ const useUploadThingInputProps = (...args: Input) => {
 export default function CustomUploadButton() {
   const { inputProps } = useUploadThingInputProps("imageUploader", {
     onUploadBegin(fileName) {
-      toast("Uploading . . .", {
-        duration: 100000,
-        id: "upload-begin",
-      });
+      toast(
+        <div className="flex items-center gap-2">
+          <LoadingSpinner /> Uploading . . .
+        </div>,
+        {
+          duration: 100000,
+          id: "upload-begin",
+        },
+      );
     },
     onClientUploadComplete() {
       toast.dismiss("upload-begin");
